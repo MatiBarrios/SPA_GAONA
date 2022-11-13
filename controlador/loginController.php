1b901321a465cc571bed95c7c8e27d738c3430fa
EnzoVirgona <enzovirgona3476@gmail.com>
2022-11-12 22:36:44 -0300

    Creación de archivos de vista (HTML)

--- controlador/loginController.php
+++ controlador/loginController.php
@@ -12,14 +12,14 @@ if (isset($_POST['user'])){
 
 }
 else if (isset($_GET['cargo'])){
-    if ($_GET['cargo'] == "Secretaria de comercialización") require_once("../vistas/secretaria.php");
-    else if ($_GET['cargo'] == "Agente inmobiliario") require_once("./vistas/login.php");
-    else if ($_GET['cargo'] == "Empleado de marketing") require_once("./vistas/login.php");
-    else if ($_GET['cargo'] == "Cajera") require_once("./vistas/login.php");
-    else if ($_GET['cargo'] == "Jefa de administración") require_once("./vistas/login.php");
-    else if ($_GET['cargo'] == "Jefa de comercialización") require_once("./vistas/login.php");
-    else if ($_GET['cargo'] == "Administrador") require_once("./vistas/login.php");
-    else if ($_GET['cargo'] == "Gerente general") require_once("./vistas/login.php");
+    if ($_GET['cargo'] == "Secretaria de comercialización") require_once("../vistas/empleados/secretaria.html");
+    else if ($_GET['cargo'] == "Agente inmobiliario") require_once("../vistas/empleados/agente.html");
+    else if ($_GET['cargo'] == "Empleado de marketing") require_once("../vistas/empleados/marketing.html");
+    else if ($_GET['cargo'] == "Cajera") require_once("../vistas/empleados/cajera.html");
+    else if ($_GET['cargo'] == "Jefa de administración") require_once("../vistas/empleados/jefa_administracion.html");
+    else if ($_GET['cargo'] == "Jefa de comercialización") require_once("../vistas/empleados/jefa_comercializacion.html");
+    else if ($_GET['cargo'] == "Administrador") require_once("../vistas/empleados/administrador.html");
+    else if ($_GET['cargo'] == "Gerente general") require_once("../vistas/empleados/gerente.html");
 }
 else require_once("./vistas/login.php");
 ?>
