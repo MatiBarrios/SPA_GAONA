5623626c21bdcd2b825bdddf163edfadb15e71f1
EnzoVirgona <enzovirgona3476@gmail.com>
2022-11-16 00:39:05 -0300

    procesado de vistas completo

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,6 +9,7 @@ const procesarJSON = (json) =>{
 }
 
 const sesion = procesarJSON(login);
+var vista_ver1 = plantilla;
 
 const iniciarSesion = (usuario, contrasenia) =>{
     event.preventDefault();
@@ -26,15 +27,47 @@ const iniciarSesion = (usuario, contrasenia) =>{
         }
         else{
             var xhr2 = new XMLHttpRequest();
-            xhr2.open("GET","./controlador/ControladorEmpleado.php");
+            xhr2.open("GET","./controlador/ControladorEmpleado.php?cargo="+json["cargo"]);
+            xhr2.responseType = "json";
             xhr2.send();
             xhr2.onloadend = ()=>{
+                var partes_vista = vista_ver1.split("<!-- Boton navbar -->");
+                vista_ver1 = partes_vista[0];
+                navbar = Object.keys(xhr2.response);
 
+                for(var x=0; x<navbar.length; x++){
+                    vista_ver1 += partes_vista[1].replace("BOTON_NAVBAR", navbar[x]).
+                    replace("CODIGO_BOTON", "procesasVista("+JSON.stringify(xhr2.response)+", "+x+", 0)");
+                }
+
+                vista_ver1 += partes_vista[2];
+                vista_ver1 = vista_ver1.replace("NOMBRE_EMPLEADO", json["nombre"]+" "+json["apellido"]);
+                vista_ver1 = vista_ver1.replace("CARGO_EMPLEADO", json["cargo"]);
+                document.getElementById("conteiner").innerHTML = vista_ver1;
+                procesasVista(xhr2.response, 0, 0);
             }
-            var vista = plantilla.replace("NOMBRE_EMPLEADO", json["nombre"]+" "+json["apellido"]);
-            vista = vista.replace("CARGO_EMPLEADO", json["cargo"]);
-            
-            document.getElementById("conteiner").innerHTML = vista;
         }
+    }
+}
+
+const procesasVista = (botones, btn_navbar, btn_sidebar) =>{
+    var partes_vista = vista_ver1.split("<!-- Boton sidebar -->");
+    var vista = partes_vista[0];
+    navbar = Object.keys(botones);
+    sidebar = Object.keys(botones[navbar[btn_navbar]]);
+
+    for(var x=0; x<sidebar.length; x++){
+        vista += partes_vista[1].replace("BOTON_SIDEBAR", sidebar[x]).
+        replace("LOGO", botones[navbar[btn_navbar]][sidebar[x]][1]).
+        replace("CODIGO_BOTON", "procesasVista("+JSON.stringify(botones)+", "+btn_navbar+", "+x+")");
+    }
+
+    vista += partes_vista[2];
+    document.getElementById("conteiner").innerHTML = vista;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",botones[navbar[btn_navbar]][sidebar[btn_sidebar]][0]);
+    xhr.send();
+    xhr.onloadend = ()=>{
+        document.getElementById("subcontainer").innerHTML = xhr.response;
     }
 }
--- controlador/ControladorEmpleado.php
+++ controlador/ControladorEmpleado.php
@@ -2,92 +2,92 @@
 if (isset($_GET['cargo'])){
     if ($_GET['cargo'] == "Secretaria de comercialización"){
         echo '{
-            "clientes":{
-                "actualizar datos":["./vistas/empleados/secretaria/actualizar_cliente.html", "update"],
-                "nuevo cliente":["./vistas/empleados/secretaria/nuevo_cliente.html", "person_add"]
+            "Clientes":{
+                "Actualizar datos":["./vistas/empleados/secretaria/actualizar_cliente.html", "update"],
+                "Nuevo cliente":["./vistas/empleados/secretaria/nuevo_cliente.html", "person_add"]
             },
-            "agenda":{
-                "mostrar agenda":["./vistas/empleados/secretaria/agenda.html", "schedule"],
-                "agendar cita":["./vistas/empleados/secretaria/agendar_cita.html", "add_box"]
+            "Agenda":{
+                "Mostrar agenda":["./vistas/empleados/secretaria/agenda.html", "schedule"],
+                "Agendar cita":["./vistas/empleados/secretaria/agendar_cita.html", "add_box"]
             },
-            "solicitudes":{
-                "solicitudes":["./vistas/empleados/secretaria/solicitudes.html", "list"]
+            "Solicitudes":{
+                "Solicitudes":["./vistas/empleados/secretaria/solicitudes.html", "list"]
             }
         }';
     }
     else if ($_GET['cargo'] == "Agente inmobiliario"){
         echo '{
-            "agenda":{
-                "agenda":["./vistas/empleados/agente/agenda.html", "schedule"]
+            "Agenda":{
+                "Agenda":["./vistas/empleados/agente/agenda.html", "schedule"]
             },
-            "propiedades":{
-                "catalogo":["./vistas/empleados/agente/propiedades.html", "house"],
-                "nueva propiedad":["./vistas/empleados/agente/nueva_propiedad.html", "add"]
+            "Propiedades":{
+                "Catalogo":["./vistas/empleados/agente/propiedades.html", "house"],
+                "Nueva propiedad":["./vistas/empleados/agente/nueva_propiedad.html", "add"]
             }
         }';
     }
     else if ($_GET['cargo'] == "Empleado de marketing"){
         echo '{
-            "propiedades":{
-                "catalogo":["./vistas/empleados/marketing/propiedades.html", "house"]
+            "Propiedades":{
+                "Catalogo":["./vistas/empleados/marketing/propiedades.html", "house"]
             }
         }';
     }
     else if ($_GET['cargo'] == "Cajera"){
         echo '{
-            "caja":{
-                "alquiler":["./vistas/empleados/cajera/caja_alquiler.html", "attach_money"],
-                "venta":["./vistas/empleados/cajera/caja_venta.html", "handshake"],
-                "cerrar caja":["./vistas/empleados/cajera/cierre_caja.html", "point_of_sale"]
+            "Caja":{
+                "Alquiler":["./vistas/empleados/cajera/caja_alquiler.html", "attach_money"],
+                "Venta":["./vistas/empleados/cajera/caja_venta.html", "handshake"],
+                "Cerrar caja":["./vistas/empleados/cajera/cierre_caja.html", "point_of_sale"]
             },
-            "movimientos":{
-                "entradas y salidas":["./vistas/empleados/cajera/entrada_salida.html", "sync_alt"],
-                "movimientos de caja":["./vistas/empleados/cajera/movimientos.html", "handshake"]
+            "Movimientos":{
+                "Entradas y salidas":["./vistas/empleados/cajera/entrada_salida.html", "sync_alt"],
+                "Movimientos de caja":["./vistas/empleados/cajera/movimientos.html", "handshake"]
             }
         }';
     }
     else if ($_GET['cargo'] == "Jefa de administración"){
         echo '{
-            "clientes":{
-                "clientes":["./vistas/empleados/jefa_de_administracion/clientes.html", "group"]
+            "Clientes":{
+                "Clientes":["./vistas/empleados/jefa_de_administracion/clientes.html", "group"]
             },
-            "transacciones":{
-                "transacciones":["./vistas/empleados/jefa_de_administracion/transacciones.html", "attach_money"],
-                "reportes":["./vistas/empleados/jefa_de_administracion/reportes.html", "list_alt"]
+            "Transacciones":{
+                "Transacciones":["./vistas/empleados/jefa_de_administracion/transacciones.html", "attach_money"],
+                "Reportes":["./vistas/empleados/jefa_de_administracion/reportes.html", "list_alt"]
             }
         }';
     }
     else if ($_GET['cargo'] == "Jefa de comercialización"){
         echo '{
-            "agenda":{
-                "agenda":["./vistas/empleados/jefa_de_comercializacion/agenda.html", "schedule"]
+            "Agenda":{
+                "Agenda":["./vistas/empleados/jefa_de_comercializacion/agenda.html", "schedule"]
             },
-            "reportes":{
-                "venta":["./vistas/empleados/jefa_de_comercializacion/reportes_venta.html", "handshake"],
-                "alquiler":["./vistas/empleados/jefa_de_comercializacion/reportes_alquiler.html", "attach_money"],
-                "clientes":["./vistas/empleados/jefa_de_comercializacion/reportes_clientes.html", "group"],
-                "propiedades":["./vistas/empleados/jefa_de_comercializacion/reportes_propiedades.html", "house"]
+            "Reportes":{
+                "Venta":["./vistas/empleados/jefa_de_comercializacion/reportes_venta.html", "handshake"],
+                "Alquiler":["./vistas/empleados/jefa_de_comercializacion/reportes_alquiler.html", "attach_money"],
+                "Clientes":["./vistas/empleados/jefa_de_comercializacion/reportes_clientes.html", "group"],
+                "Propiedades":["./vistas/empleados/jefa_de_comercializacion/reportes_propiedades.html", "house"]
             },
-            "clientes":{
-                "clientes":["./vistas/empleados/jefa_de_comercializacion/clientes.html", "group"]
+            "Clientes":{
+                "Clientes":["./vistas/empleados/jefa_de_comercializacion/clientes.html", "group"]
             },
-            "propiedades":{
-                "catalogo":["./vistas/empleados/jefa_de_comercializacion/propiedades.html", "house"]
+            "Propiedades":{
+                "Catalogo":["./vistas/empleados/jefa_de_comercializacion/propiedades.html", "house"]
             }
         }';
     }
     else if ($_GET['cargo'] == "Administrador"){
         echo '{
-            "empleados":{
-                "agregar":["./vistas/empleados/administrador/agregar_empleado.html", "person_add"],
-                "deshabilitar":["./vistas/empleados/administrador/deshabilitar_empleado.html", "person_remove"]
+            "Empleados":{
+                "Agregar":["./vistas/empleados/administrador/agregar_empleado.html", "person_add"],
+                "Deshabilitar":["./vistas/empleados/administrador/deshabilitar_empleado.html", "person_remove"]
             }
         }';
     }
     else if ($_GET['cargo'] == "Gerente general"){
         echo '{
-            "reportes":{
-                "reportes":["./vistas/empleados/gerente/reportes.html", "monitoring"]
+            "Reportes":{
+                "Reportes":["./vistas/empleados/gerente/reportes.html", "monitoring"]
             }
         }';
     }
